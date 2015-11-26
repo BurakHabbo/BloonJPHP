f314f2778f91b7083f5951d1cecbf960d5ebfc4a
Burak <burak@burak.fr>
2015-11-26 16:17:51 +0100

    Work on WebServerAPI, implement token and whitelist check

--- src/class/WebServerAPI.php
+++ src/class/WebServerAPI.php
@@ -22,16 +22,48 @@ class WebServerAPI {
     private $whitelist = array();
 
     public function __construct($tokenEnabled = true, $token = "", $whitelistEnabled = true, $whitelist = "") {
-        $this->tokenEnabled = $tokenEnabled;
-        $this->token = explode(";", $token);
-        $this->whitelistEnabled = $whitelistEnabled;
-        $this->whitelist = explode(";", $whitelist);
-
         $this->server = new WebServer(function(WebRequest $req, WebResponse $res) {
-            echo "Hello World!";
-            //TODO: Token check, Whitelist check, Router, Controller, Render
+            $current = WebServer::current();
+            $drop = false;
+
+            if ($current->tokenEnabled) {
+                $drop = true;
+
+                foreach ($req->cookies as $cookie) {
+                    if (strtolower($cookie['name']) == "token" && in_array($cookie['value'], $current->token)) {
+                        $drop = false;
+                    }
+                }
+            }
+
+            if ($current->whitelistEnabled && !in_array($req->ip, $current->whitelist))
+                $drop = true;
+
+            if (!$drop) {
+                echo "Hello World!\n";
+                //TODO: Router, Controller, Render
+
+                /* Some debug data */
+                var_dump($req->method);
+                var_dump($req->scheme);
+                var_dump($req->pathInfo);
+                var_dump($req->servletPath);
+                var_dump($req->queryString);
+                var_dump($req->authType);
+                var_dump($req->url);
+                var_dump($req->port);
+                var_dump($req->ip);
+                var_dump($req->cookies);
+            } else {
+                $res->status = 403;
+                echo "Access denied";
+            }
         });
 
+        $this->server->tokenEnabled = $this->tokenEnabled = $tokenEnabled;
+        $this->server->token = $this->token = explode(";", $token);
+        $this->server->whitelistEnabled = $this->whitelistEnabled = $whitelistEnabled;
+        $this->server->whitelist = $this->whitelist = explode(";", $whitelist);
         $this->server->isolated = true;
         $this->server->hotReload = true;
     }
